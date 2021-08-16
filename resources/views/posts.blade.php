fa5cfe1d3ced7454811728d158b37361e9dc7414
Hanna Herasimava <annlisandias@gmail.com>
2021-08-17 00:06:09 +0200

    Use FileSystem Class for Posts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,17 +9,10 @@
     <script src="/app.js"></script>
 </head>
 <body>
+   <?php foreach($posts as $post) : ?>
    <article>
-       <h1><a href="/posts/my-first-post">My First Post</a></h1>
-       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis, iure quas accusamus expedita ipsam cupiditate et eum reiciendis nisi aperiam, voluptatem vero sint perferendis! Aspernatur magni adipisci odio nihil inventore.</p>
-    </article>
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis, iure quas accusamus expedita ipsam cupiditate et eum reiciendis nisi aperiam, voluptatem vero sint perferendis! Aspernatur magni adipisci odio nihil inventore.</p>
-     </article>
-     <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis, iure quas accusamus expedita ipsam cupiditate et eum reiciendis nisi aperiam, voluptatem vero sint perferendis! Aspernatur magni adipisci odio nihil inventore.</p>
-     </article>
+      <?= $post; ?>
+   </article>
+   <?php endforeach ?>
 </body>
 </html>
